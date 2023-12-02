eba52888f7bfa96f7fa72b25669359adc7420206
Nick Koldys <46114692+koldysnk@users.noreply.github.com>
2023-12-02 17:34:57 -0500

    Update day2part2.py made twice as fast

--- day2/day2part2.py
+++ day2/day2part2.py
@@ -1,5 +1,5 @@
-
-INPUT_FILE = "day2/day2input.txt"
+import time
+INPUT_FILE = "Day2/day2input.txt"
 
 
 
@@ -13,7 +13,7 @@ def attempt1():
         ids = line[:colon]
         games = line[colon+1:].strip().split(";")
         successful = True
-        minimum = {
+        maximum = {
             "red":0,
             "green":0,
             "blue":0
@@ -24,16 +24,52 @@ def attempt1():
                 temp = pull.split()
                 num = int(temp[0])
                 color = temp[1]
-                minimum[color] = max(minimum[color],num)
+                maximum[color] = max(maximum[color],num)
         line = f.readline()
-        total += minimum["red"]*minimum["green"]*minimum["blue"]
-        print(total)
+        total += maximum["red"]*maximum["green"]*maximum["blue"]
+    print(total)
+    return total
+
+#About twice as fast
+def attempt2():
+    f = open(INPUT_FILE,"r")
+    total = 0
+    line = f.read()
+    line = line.replace('\n'," ").replace(",","").replace(";",'').replace(":","").split()
+    i = 0
+    current = {
+        "red":0,
+        "blue":0,
+        "green":0
+    }
+    while i < len(line):
+        if line[i] == "Game":
+            total += current["red"]*current["blue"]*current["green"]
+            
+            current = {
+                "red":0,
+                "blue":0,
+                "green":0
+            }
+        else:
+            color = line[i+1]
+            num = int(line[i])
+            current[color] = max(num,current[color])
+        i +=2
+    total += current["red"]*current["blue"]*current["green"]
     print(total)
     return total
 
 
 def main():
+    tic = time.perf_counter()
     attempt1()
+    toc = time.perf_counter()
+    print(f"Attempt 1 took {toc-tic} seconds")
+    tic = time.perf_counter()
+    attempt2()
+    toc = time.perf_counter()
+    print(f"Attempt 2 took {toc-tic} seconds")
 
 
 main()
