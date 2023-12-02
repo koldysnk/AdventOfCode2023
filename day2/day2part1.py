e671a595347584d53b47b0a91de70eb9af865539
Nick Koldys <46114692+koldysnk@users.noreply.github.com>
2023-12-02 17:35:28 -0500

    Update day2part1.py made twice as fast

--- day2/day2part1.py
+++ day2/day2part1.py
@@ -1,4 +1,4 @@
-
+import time
 INPUT_FILE = "Day2/day2input.txt"
 
 KEY = {
@@ -25,20 +25,50 @@ def attempt1():
                 num = temp[0]
                 color = temp[1]
                 if color not in KEY or KEY[color] < int(num):
-                    print("fail")
                     successful = False
-                else:
-                    print("pass")
         line = f.readline()
         if successful:
             total += int(ids)
-        print(total)
+    print(total)
+    return total
+
+#About twice as fast
+def attempt2():
+    f = open(INPUT_FILE,"r")
+    total = 0
+    line = f.read()
+    line = line.replace('\n'," ").replace(",","").replace(";",'').replace(":","").split()
+    game_id = 0
+    success = True
+    i = 0
+    while i < len(line):
+        if line[i] == "Game":
+            if success:
+                total += game_id
+            success = True
+            game_id = int(line[i+1])
+        else:
+            color = line[i+1]
+            num = line[i]
+            if KEY[color] < int(num):
+                success = False
+        i += 2
+            
+
     print(total)
     return total
 
 
+
 def main():
+    tic = time.perf_counter()
     attempt1()
+    toc = time.perf_counter()
+    print(f"Attempt 1 took {toc-tic} seconds")
+    tic = time.perf_counter()
+    attempt2()
+    toc = time.perf_counter()
+    print(f"Attempt 2 took {toc-tic} seconds")
 
 
 main()
