83d8eab2f326f97e3009dd1bdd35f2930b9a23b7
Nick Koldys <46114692+koldysnk@users.noreply.github.com>
2023-12-10 01:29:38 -0500

    Create day10part2.py

---
+++ day10/day10part2.py
@@ -0,0 +1,67 @@
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
+
+INPUT_FILE = "Day10/day10input.txt"
+f = open(INPUT_FILE,"r")
+
+line = f.readline()
+sewer = []
+xS=0
+yS=0
+found = False
+while line:
+    row = list(line.replace("\n",""))
+    if "S" in row:
+        yS = row.index("S")
+        found = True
+    if not found:
+        xS+=1
+    sewer.append(row)
+    line=f.readline()
+
+done = False
+steps = 0
+UP, RIGHT, DOWN, LEFT = 0,1,2,3
+last_move = -1
+x,y = xS,yS
+p = "S"
+loop = []
+
+while not done:
+    loop.append((x,y))
+
+    #Can move anywhere
+    if p == "S" and last_move >=0:
+            done = True
+    #Move up
+    elif last_move != DOWN and p in ["S","|","L","J"] and x > 0 and sewer[x-1][y] in ["|","7","F","S"]:
+        last_move = UP
+        steps +=1
+        x -=1
+        p = sewer[x][y]
+    elif last_move != LEFT and p in ["S","-","L","F"] and y < len(sewer[x])-1 and sewer[x][y+1] in ["-","7","J","S"]:
+        last_move = RIGHT
+        steps +=1
+        y +=1
+        p = sewer[x][y]
+    elif last_move != UP and p in ["S","|","7","F"] and x < len(sewer)-1 and sewer[x+1][y] in ["|","L","J","S"]:
+        last_move = DOWN
+        steps +=1
+        x +=1
+        p = sewer[x][y]
+    elif last_move != RIGHT and p in ["S","-","7","J"] and y > 0 and sewer[x][y-1] in ["-","L","F","S"]:
+        last_move = LEFT
+        steps +=1
+        y -=1
+        p = sewer[x][y]
+print(steps)
+
+polygon = Polygon(loop)
+total = 0
+for x in range(len(sewer)):
+    for y in range(len(sewer[x])):
+        if (x,y) not in loop:
+            inside = polygon.contains(Point(x,y))
+            total += int(inside)
+            print(f"{x},{y} -> {inside}")
+print(total)
